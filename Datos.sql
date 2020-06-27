72466a64cb0950957087118f43e2ee1a284cce47
MarceRezzano <marcerezzano@gmail.com>
2020-06-27 16:28:49 -0300

    - Cambios en relacion de partida con tablero y pos partida - Set de datos agregados

---
+++ Datos.sql
@@ -0,0 +1,15 @@
+--DATOS TERRENO
+insert into terreno values (1,'T');
+insert into terreno values (2,'A');
+insert into terreno values (3,'.');
+insert into terreno values (4,'P');
+insert into terreno values (5,'B');
+
+SELECT * FROM TERRENO;
+
+--DATOS TABLERO 15X150
+INSERT  INTO TABLERO  values (1,1,1);
+INSERT  INTO TABLERO  values (1,1,2);
+INSERT  INTO TABLERO  values (1,1,3);
+
+SELECT * FROM TABLERO
